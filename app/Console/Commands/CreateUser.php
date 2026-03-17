0b42e8916f871bb5058a28c87257b26aed37e843
Ringo Leese <ringo19@gmx.de>
2026-03-17 21:58:23 +0100

    Added CLI for creating a new user.

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+#[Signature('user:create')]
+#[Description('Legt einen neuen Benutzer an')]
+class CreateUser extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $this->info('Neuen Benutzer anlegen');
+        $this->line('----------------------------');
+
+        $name = $this->ask('Name');
+        $email = $this->ask('E-Mail');
+        $password = $this->secret('Passwort');
+        $passwordConfirm = $this->secret('Passwort bestätigen');
+
+        $validator = Validator::make(
+            [
+                'name'                  => $name,
+                'email'                 => $email,
+                'password'              => $password,
+                'password_confirmation' => $passwordConfirm,
+            ],
+            [
+                'name'     => ['required', 'string', 'max:255'],
+                'email'    => ['required', 'email', 'unique:users,email'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+
+            return self::FAILURE;
+        }
+
+        User::create([
+            'name'     => $name,
+            'email'    => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->info("Benutzer \"$name\" ($email) wurde erfolgreich angelegt.");
+
+        return self::SUCCESS;
+    }
+}
