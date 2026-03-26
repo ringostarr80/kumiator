6aa71fd7e88ea9554fc678824972169fdbf917c9
Ringo Leese <ringo19@gmx.de>
2026-03-26 13:15:47 +0100

    Added tests for CreateRole command.

---
+++ tests/Feature/CreateRoleCommandTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\PendingCommand;
+use Spatie\Permission\Models\Role;
+use Tests\TestCase;
+
+class CreateRoleCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testRoleCanBeCreated(): void
+    {
+        $command = $this->artisan('role:create');
+        assert($command instanceof PendingCommand);
+
+        $command
+            ->expectsQuestion(__('commands.create_role.ask_name'), 'admin')
+            ->expectsOutputToContain(__('commands.create_role.success', ['name' => 'admin']))
+            ->assertSuccessful()
+            ->run();
+
+        $this->assertTrue(Role::where('name', 'admin')->exists());
+    }
+
+    public function testRoleCreationFailsWithEmptyName(): void
+    {
+        $command = $this->artisan('role:create');
+        assert($command instanceof PendingCommand);
+
+        $command
+            ->expectsQuestion(__('commands.create_role.ask_name'), '')
+            ->assertFailed()
+            ->run();
+    }
+
+    public function testRoleCreationFailsWithDuplicateName(): void
+    {
+        Role::findOrCreate('admin');
+
+        $command = $this->artisan('role:create');
+        assert($command instanceof PendingCommand);
+
+        $command
+            ->expectsQuestion(__('commands.create_role.ask_name'), 'admin')
+            ->assertFailed()
+            ->run();
+    }
+}
