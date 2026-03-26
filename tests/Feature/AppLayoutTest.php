dcc65be523aa3dc008d15c00daeaacbd21e203b9
Ringo Leese <ringo19@gmx.de>
2026-03-26 19:35:36 +0100

    Added tests for AppLayout view.

---
+++ tests/Feature/AppLayoutTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\View\Components\AppLayout;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\View\View;
+use Tests\TestCase;
+
+class AppLayoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAppLayoutRendersCorrectView(): void
+    {
+        $component = new AppLayout();
+        $view = $component->render();
+
+        $this->assertInstanceOf(View::class, $view);
+        $this->assertEquals('layouts.app', $view->name());
+    }
+}
