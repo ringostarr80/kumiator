8a9ab898be88a88332523eb9fc565658a76b4645
Ringo Leese <ringo19@gmx.de>
2026-03-26 19:32:54 +0100

    Added tests for LocaleController.

---
+++ tests/Feature/LocaleControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LocaleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testLocaleCanBeSwitchedToEnglish(): void
+    {
+        $response = $this->get(route('locale.switch', ['locale' => 'en']));
+
+        $response->assertRedirect();
+        $this->assertEquals('en', session('locale'));
+    }
+
+    public function testLocaleCanBeSwitchedToGerman(): void
+    {
+        $response = $this->get(route('locale.switch', ['locale' => 'de']));
+
+        $response->assertRedirect();
+        $this->assertEquals('de', session('locale'));
+    }
+
+    public function testUnsupportedLocaleIsIgnored(): void
+    {
+        session(['locale' => 'de']);
+
+        $response = $this->get(route('locale.switch', ['locale' => 'fr']));
+
+        $response->assertRedirect();
+        $this->assertEquals('de', session('locale'));
+    }
+}
