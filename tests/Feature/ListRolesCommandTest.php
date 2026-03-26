1e1646becdbb2107f5072506deead4034f849e52
Ringo Leese <ringo19@gmx.de>
2026-03-26 14:05:26 +0100

    Added tests for ListRoles command.

---
+++ tests/Feature/ListRolesCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\PendingCommand;
+use Spatie\Permission\Models\Role;
+use Tests\TestCase;
+
+class ListRolesCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testRolesAreListed(): void
+    {
+        Role::findOrCreate('admin');
+        Role::findOrCreate('member');
+
+        $command = $this->artisan('role:list');
+        assert($command instanceof PendingCommand);
+
+        $command
+            ->expectsOutputToContain('admin')
+            ->expectsOutputToContain('member')
+            ->expectsOutputToContain(__('commands.list_roles.total', ['count' => 2]))
+            ->assertSuccessful()
+            ->run();
+    }
+
+    public function testNoRolesShowsInfoMessage(): void
+    {
+        $command = $this->artisan('role:list');
+        assert($command instanceof PendingCommand);
+
+        $command
+            ->expectsOutputToContain(__('commands.list_roles.no_roles'))
+            ->assertSuccessful()
+            ->run();
+    }
+}
